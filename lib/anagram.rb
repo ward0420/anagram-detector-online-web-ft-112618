8084cffbbe344e8b4ffafade29994362b63f3fbf
Don Ward <10is0420@gmail.com>
2019-01-03 21:46:24 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require "pry"
+class Anagram
+  
+  attr_accessor = :diaper
+  
+  def initialize(diaper)
+    
+    @diaper = diaper
+    
+  end
+  
+  def match(diaper)
+    
+   #@diaper.split(//).sort == diaper.split(//).sort
+    diaper.select{ |i| i.split(//).sort == @diaper.split(//).sort}
+   
+    
+  end
+  
+end
